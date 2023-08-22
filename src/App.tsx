0446f62a401c8d49644fc48c2fa7c9b40e1e2488
jenya-b <evgeny_rab@inbox.ru>
2023-08-22 15:49:54 +0300

    feat: add list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, CSSProperties } from 'react';
 import { useAppDispatch, useAppSelector } from './store';
 import { loadData } from './store/app/thunks';
+import { List } from './components/List';
 
 export const App: React.FC = () => {
   const dispatch = useAppDispatch();
-  const isDataLoaded = useAppSelector((state) => state.app.isDataLoaded);
+  const { isDataLoaded, elements } = useAppSelector((state) => state.app);
 
   useEffect(() => {
     dispatch(loadData()).unwrap();
   }, []);
 
-  return <>
-    <p>{isDataLoaded ? 'Done' : 'Loading'}</p>
-  </>;
+  const renderItem: React.FC<{ index: number; style: CSSProperties }> = ({
+    index,
+    style,
+  }) => <div style={style}>{elements[index]}</div>;
+
+  return (
+    <div style={{ height: '100%' }}>
+      <p>{isDataLoaded ? 'Done' : 'Loading'}</p>
+      <List data={elements} renderItem={renderItem} />
+    </div>
+  );
 };
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,1 +1,23 @@
-// TODO
+import React, { CSSProperties } from 'react';
+import { FixedSizeList } from 'react-window';
+
+interface ListProps {
+  data: string[];
+  renderItem: React.FC<{
+    index: number;
+    style: CSSProperties;
+  }>;
+}
+
+export const List: React.FC<ListProps> = ({ data = [], renderItem }) => {
+  return (
+    <FixedSizeList
+      height={window.innerHeight}
+      width={window.innerWidth}
+      itemCount={data.length}
+      itemSize={50}
+    >
+      {renderItem}
+    </FixedSizeList>
+  );
+};
